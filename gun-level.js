d64ffee220a5da7f466d0f764bda5548b8ad34cd
Jesse Gibson <Jesse_Gibson@me.com>
2015-11-10 14:34:25 -0700

    Allow instances to share a database, data is now scoped to a folder instead of an instance, fix async folder detection, graph data support for all methods, code quality improvements, export Gun for brevity.
    Level would sometimes throw an error because Gun instances were trying to open a database that was already open. This has been fixed, and now Gun instances can share and write to the same folder (as they link up to the same level controller).
    
    In the last version, I had this silly idea to scope data to an instance inside of the target folder. Yeah. We changed that.
    
    The last version had another bug where I was trying to detect whether a folder existed asynchronously and, if not, create it asynchronously. That caused some major issues. After reading more documentation, I was able to remove all folder logic completely, leaving the asynchronous bits to level. This will probably be a future feature where I'll let the folder option contain variable depth and blaze a path to it.
    
    In the previous version, `.key` and `.get` weren't holding graph-style data. That's been fixed, so now your key can refer to a collection, and `.get` won't send an error telling of bad data.
    
    The code before would've made a maggot puke. Now they should find it tolerable.
    
    And now, Gun is exported for brevity. You can require `gun-level` and save it as the Gun constructor, instead of making two `require` calls. Yay!

--- gun-level.js
+++ gun-level.js
@@ -4,108 +4,176 @@
 var Gun = require('gun'),
   fs = require('fs'),
   levelUP = require('level'),
-  defaultFolder = null;
+  defaultFolder = 'level/';
 
-function ID() {
-  this.id = Gun.text.random();
+var path = {};
+
+
+function valid(err) {
+  var noData = 'Key not found in database';
+
+  if (!err) {
+    return false;
+  }
+  if (err.message.match(noData)) {
+    return false;
+  }
+  return true;
 }
 
 
-function setup(gun, opt) {
+function patch(opt) {
+  // Give default options where they aren't defined
+
   opt = opt || {};
+  opt.hooks = opt.hooks || {};
   opt.level = opt.level || {};
-  var level, quit, id, folder = (opt.level.folder || defaultFolder);
-  folder = folder && folder.replace(/\/$/, '') + '/';
 
-  if (!gun.__.level) {
-    gun.__.level = new ID();
-  }
-  id = gun.__.level.id;
+  var folder, level = opt.level;
+  opt.level.folder = opt.level.folder || defaultFolder;
+  folder = level.folder;
+  opt.level.folder = folder && folder.replace(/\/$/, '') + '/';
 
-  if (folder === null) {
-    folder = 'level/';
-    fs.exists(folder, function (exists) {
-      if (exists) {
-        defaultFolder = folder;
-        return setup(gun, opt);
-      }
-      fs.mkdir(folder, function () {
-        defaultFolder = folder;
-        setup(gun, opt);
-      });
+  return opt;
+}
+
+
+
+function setup(gun, opt) {
+
+  opt = patch(opt);
+
+  var driver, level, folder = opt.level.folder;
+
+
+  // level instances can't share a database
+  // set it to the instance already using that path.
+  if (!path[folder]) {
+
+    path[folder] = level = levelUP(folder, {
+      valueEncoding: 'json'
     });
-    return;
+
+  } else {
+    level = path[folder];
   }
 
-  level = levelUP(folder + id, {
-    valueEncoding: 'json'
-  });
 
-  function get(key, cb, opt) {
-    if (!key) {
-      return cb({
-        err: "No key was given to .get()"
-      });
-    }
-    level.get(key, function (err, graph) {
-      if (err) {
-        cb({
-          err: err
+
+
+  driver = {
+    get: function (key, cb, opt) {
+
+      if (!key) {
+        return cb({
+          err: "No key was given to .get()"
         }, false);
-      } else {
-        cb(null, graph);
       }
-    });
-  }
 
-  function put(graph, cb, opt) {
-    var saved = 0,
-      pending = 0;
+      level.get(key, function (err, souls) {
+        var saved = 0,
+          pending = 0;
 
-    Gun.is.graph(graph, function (node, soul) {
-      pending += 1;
-      level.put(soul, node, function (err) {
-        if (err) {
-          cb({
+        if (!souls) {
+          cb(null, null);
+        }
+
+        Gun.obj.map(souls, function (rel, soul) {
+          pending += 1;
+
+          level.get(soul, function (err, node) {
+            if (valid(err)) {
+              return cb({
+                err: err
+              }, false);
+            }
+            var graph = {},
+              soul = Gun.is.soul.on(node);
+            graph[soul] = node;
+            cb(null, graph || null);
+            graph = {};
+            graph[soul] = Gun.union.pseudo(soul);
+            cb(null, graph || null);
+
+            saved += 1;
+            if (pending === saved) {
+              cb(null, {});
+            }
+          });
+        });
+      });
+
+    },
+
+    put: function (graph, cb, opt) {
+      var saved = 0,
+        pending = 0;
+
+      Gun.is.graph(graph, function (node, soul) {
+        pending += 1;
+        level.put(soul, node, function (err) {
+          if (valid(err)) {
+            cb({
+              err: err
+            }, false);
+          }
+          saved += 1;
+          if (pending === saved) {
+            cb(null, true);
+          }
+        });
+      });
+
+    },
+
+    key: function (name, soul, cb) {
+      if (!name) {
+        return cb({
+          err: "No key was given to .key()"
+        }, false);
+      }
+      if (!soul) {
+        return cb({
+          err: "No soul given to .key()"
+        }, false);
+      }
+      level.get(name, function (err, graph) {
+        if (valid(err)) {
+          return cb({
             err: err
           }, false);
         }
-        saved += 1;
-        if (pending === saved) {
+        graph = graph || {};
+        graph[soul] = {
+          '#': soul
+        };
+        level.put(name, graph, function (err) {
+          if (valid(err)) {
+            return cb({
+              err: err
+            }, false);
+          }
           cb(null, true);
-        }
+        });
+
       });
-    });
-  }
 
-  function key(name, soul, cb) {
-    if (!name) {
-      return cb({
-        err: "No key was given to .key()"
-      }, false);
     }
-    if (!soul) {
-      return cb({
-        err: "No soul given to .key()"
-      }, false);
-    }
-    level.put(name, soul, function (err) {
-      if (err) {
-        cb({
-          err: err
-        }, false);
-      } else {
-        cb(null, true);
-      }
-    });
-  }
+  };
 
-  opt.hooks = opt.hooks || {};
+
+
+
+
+
+
+
+
   gun.opt({
     hooks: {
-      get: opt.hooks.get || get,
-      put: opt.hooks.put || put,
-      key: opt.hooks.key || key
+      get: opt.hooks.get || driver.get,
+      put: opt.hooks.put || driver.put,
+      key: opt.hooks.key || driver.key
     }
   }, true);
 
@@ -113,3 +181,6 @@ function setup(gun, opt) {
 
 
 Gun.on('opt').event(setup);
+
+
+module.exports = Gun;
