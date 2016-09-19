fce7ed8e41b2160b37ef98dc1244514b5fb551dd
SwhGo_oN <swhgoon@users.noreply.github.com>
2016-09-19 16:20:30 +0800

    adapter and wire's handles work at the same time
    Tried with code:)
    ```
    var gun = Gun({
      file : "db/db.json",
      level: levelup('db/down.json', {db: require('jsondown')})
    });
    ```

--- src/index.js
+++ src/index.js
@@ -13,12 +13,18 @@ Gun.on('opt').event((gun, options) => {
 	const adapter = Adapter.from(level);
 
 	const { wire } = gun.__.opt;
+	var wireGet = wire.get
+	var wirePut = wire.put
 
 	// Register the driver.
 	gun.opt({
 		wire: {
-			get: wire.get || adapter.read,
-			put: wire.put || adapter.write,
+		      get: function (lex, cb, o) {
+			wireGet(lex, cb, o) || adapter.read(lex, cb)
+		      },
+		      put: function (graph, cb, o) {
+		        wirePut(graph, cb, o) || adapter.write(graph, cb)
+		      }
 		},
 	}, true);
 
