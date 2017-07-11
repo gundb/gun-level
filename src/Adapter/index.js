17dd15151f3cc9e1f3fe6a9c5a66d18ce15c3707
sjones6 <jones.spencera@gmail.com>
2017-07-11 08:56:25 -0400

    Add union method to replace HAM.union

--- src/Adapter/index.js
+++ src/Adapter/index.js
@@ -6,7 +6,19 @@ const options = {
   valueEncoding: 'json',
 };
 
-const { union } = Gun.HAM;
+const union = function union(vertex, node, opt){
+	if(!node || !node._){ return }
+	vertex = vertex || Gun.state.to(node);
+	if(!vertex || !vertex._){ return }
+	opt = Gun.num.is(opt)? {machine: opt} : {machine: Gun.state()};
+	opt.union = Gun.obj.copy(vertex); // Slow performance.
+	if(!Gun.node.is(node, function(val, key){
+		var HAM = Gun.HAM(opt.machine, Gun.state.is(node, key), Gun.state.is(vertex, key, true), val, vertex[key]);
+		if(!HAM.incoming){ return }
+		Gun.state.to(node, key, opt.union);
+	})){ return }
+	return opt.union;
+}
 
 /**
  * Read/write hooks for Gun.
